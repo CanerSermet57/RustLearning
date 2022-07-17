817bb7d7d06069bbf820f3d74a77de999d4ff7df
Caner <canersermet2@gmail.com>
2022-07-17 20:05:54 +0200

    minigrep first 3 section

---
+++ Examples/minigrep/target/debug/deps/minigrep-5c13a30ffea911a5.d
@@ -0,0 +1,5 @@
+C:\Users\Caner\CLionProjects\RustLearning\Examples\minigrep\target\debug\deps\minigrep-5c13a30ffea911a5.exe: src\main.rs
+
+C:\Users\Caner\CLionProjects\RustLearning\Examples\minigrep\target\debug\deps\minigrep-5c13a30ffea911a5.d: src\main.rs
+
+src\main.rs:
---
+++ Examples/minigrep/target/debug/deps/minigrep-f818205fbd8ab255.d
@@ -0,0 +1,5 @@
+C:\Users\Caner\CLionProjects\RustLearning\Examples\minigrep\target\debug\deps\minigrep-f818205fbd8ab255.exe: src\main.rs
+
+C:\Users\Caner\CLionProjects\RustLearning\Examples\minigrep\target\debug\deps\minigrep-f818205fbd8ab255.d: src\main.rs
+
+src\main.rs:
